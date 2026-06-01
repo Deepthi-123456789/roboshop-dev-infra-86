63ab2a04cd330f16e16b22fec9004fd33e5b9afa
Deepthi <deepthi121416@gmail.com>
2026-06-01 19:33:15 +0530

    terraform-infra

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.45.0"
+    }
+  }
+   backend "s3" {
+    bucket = "remote-state-86s-dev"
+    key    = "roboshop-dev-vpc"
+    #use_lockfile = true
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
