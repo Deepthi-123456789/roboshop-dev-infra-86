0e4dcbda639015cdc3c965e28a591a088af64c75
Deepthi <deepthi121416@gmail.com>
2026-06-12 13:35:35 +0530

    roboshop-dev-databases

---
+++ 40-databases/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z08145636IB1CROA8270"
+}
+
+variable "domain_name" {
+    default = "sitaram.icu"
+}
