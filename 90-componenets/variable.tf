8d897bd4a89f147b324438ae8fd07cf62c46a476
Deepthi <deepthi121416@gmail.com>
2026-06-29 09:51:22 +0530

    componenets-module

--- 90-componenets/variable.tf
+++ 90-componenets/variable.tf
@@ -1,10 +1,30 @@
-variable "project" {
-    default = "roboshop"
-    type = string
+variable "component" {
+    default = "catalogue"
 }
 
-variable "environment" {
-    default = "dev"
-    type = string
+variable "rule_priority" {
+    default = 10
 }
 
+variable "components" {
+    default = {
+        catalogue = {
+            rule_priority = 10
+        }
+        user = {
+            rule_priority = 20
+        }
+        cart = {
+            rule_priority = 30
+        }
+        shipping = {
+            rule_priority = 40
+        }
+        payment = {
+            rule_priority = 50
+        }
+        frontend = {
+            rule_priority = 10
+        }
+    }
+}
