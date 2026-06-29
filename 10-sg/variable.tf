4b55dc606d825efec0ca34e55f1c513c9a5afafd
Deepthi <deepthi121416@gmail.com>
2026-06-29 08:14:45 +0530

    terraform-infra

--- 10-sg/variable.tf
+++ 10-sg/variable.tf
@@ -6,7 +6,7 @@ variable "environment" {
     default = "dev"
 }
 
-variable sg_name {
+variable "sg_name" {
     default = ["mongodb","mysql","rabbitmq","redis","catalogue","user","cart","shipping","payment","frontend","bastion","frontend_lb","backend_lb"]
 }
 
